e82aab6c0ad422b50fd87c310fac1b145914ed6e
Yak <yassinekhayri123@gmail.com>
2024-08-12 09:31:03 +0100

    Done

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,19 +6,18 @@
 /*   By: ykhayri <ykhayri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/11 17:33:42 by ykhayri           #+#    #+#             */
-/*   Updated: 2024/08/12 08:45:23 by ykhayri          ###   ########.fr       */
+/*   Updated: 2024/08/12 09:30:46 by ykhayri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <sys/_types/_size_t.h>
 
 template <typename T>
 void iterFn(T &a) {
 	std::cout << a << std::endl;
 }
 
-template <typename T> void iter(const T *arr, int len, void (*f)(const T&)) {
+template <typename T> void iter(const T *arr, unsigned int len, void (*f)(const T&)) {
 	for (int i = 0; i < len; i++)
 		f(arr[i]);
 }
